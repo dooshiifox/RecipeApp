e5c9c7a1c9c637a927b5422b920505d4bcfc8c40
EDoosh <edoosh.yt@gmail.com>
2022-06-29 17:33:40 +1200

    Fix bugs in Weekly cacher

--- server/src/v1/utils/weekly.rs
+++ server/src/v1/utils/weekly.rs
@@ -3,7 +3,7 @@ use crate::v1::types::Date;
 use crate::v1::utils::collection::*;
 use mongodb::options::FindOneOptions;
 use mongodb::{bson::doc, Client};
-use tracing::{error, trace};
+use tracing::{debug, error, trace};
 
 /// Expires after an hour. This needs to be in milliseconds.
 const CACHE_EXPIRATION: u64 = 1000 * 60 * 60;
@@ -23,15 +23,17 @@ impl WeeklyRecipeGetter {
     /// If the weekly recipe has not been retrieved from the database or the
     /// cache has expired, will retrieve the weekly recipe from the database.
     /// Else, will return what is cached.
-    pub async fn get(&mut self, client: Client) -> Option<&Recipe> {
+    pub async fn get(&mut self, client: &Client) -> Result<&Recipe, String> {
         // If the weekly recipe has not been retrieved yet, retrieve it.
         if self.recipe.is_none() || self.is_cache_expired() {
             trace!("Weekly cache expired.");
-            self.update(client).await;
+            self.update(client).await?;
         }
 
         // Return the weekly recipe.
-        self.recipe.as_ref()
+        self.recipe
+            .as_ref()
+            .ok_or_else(|| "No weekly recipe found.".to_string())
     }
 
     /// Checks if the cache is expired.
@@ -41,7 +43,7 @@ impl WeeklyRecipeGetter {
 
     /// Retrieves and updates the weekly recipe, ignoring whether the
     /// cache is valid or not.
-    pub async fn update(&mut self, client: Client) {
+    pub async fn update(&mut self, client: &Client) -> Result<(), String> {
         trace!("Updating weekly recipe cache.");
         let db = client.get_collection::<Recipe>(Collections::Recipes);
 
@@ -51,7 +53,9 @@ impl WeeklyRecipeGetter {
             .build();
         let recipe = db
             .find_one(
-                doc! {"weekly_timestamp": { "$lt": Date::now() }},
+                // Not implemented for u64 but *is* implemented for i64,
+                // hence the conversion here.
+                doc! {"weeklyTimestamp": { "$lt": Date::now().ms() as i64 }},
                 find_options,
             )
             .await;
@@ -59,12 +63,15 @@ impl WeeklyRecipeGetter {
             Ok(Some(recipe)) => {
                 self.recipe = Some(recipe);
                 self.last_checked = Date::now();
+                Ok(())
             }
             Ok(None) => {
                 error!("No weekly recipe found in database");
+                Err("No weekly recipe found in database".to_string())
             }
             Err(err) => {
                 error!("Error getting weekly recipe from database: {}", err);
+                Err("Error getting weekly recipe from database".to_string())
             }
         }
     }
