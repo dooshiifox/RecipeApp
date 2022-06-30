e440df99cb443bb7e2fa06fbf3ef12c1df3c7061
EDoosh <edoosh.yt@gmail.com>
2022-06-30 12:45:12 +1200

    Added TailwindCSS and web fonts.

--- web/src/routes/index.svelte
+++ web/src/routes/index.svelte
@@ -3,7 +3,6 @@
 </script>
 
 <script lang="ts">
-	import Counter from '$lib/Counter.svelte';
 </script>
 
 <svelte:head>
@@ -12,50 +11,5 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
+	<h1 class="text-4xl font-extrabold">This is the site content!</h1>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
---
+++ web/tailwind.config.cjs
@@ -0,0 +1,29 @@
+const colors = require('tailwindcss/colors');
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+// Colours that don't cause Tailwind to complain about old colour names
+let warnlessColors = {};
+for (let color in colors) {
+	if (!['lightBlue', 'warmGray', 'trueGray', 'coolGray', 'blueGray'].includes(color)) {
+		warnlessColors[color] = colors[color];
+	}
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	// We should be able to manually toggle it.
+	darkMode: 'class',
+	theme: {
+		fontFamily: {
+			sans: ['Louis George Cafe', ...defaultTheme.fontFamily.sans],
+			serif: ['Merriweather', 'serif'],
+			mono: ['droid-sans-mono-slashed', ...defaultTheme.fontFamily.mono]
+		},
+		colors: {
+			...warnlessColors
+		},
+		extend: {}
+	},
+	plugins: [require('@tailwindcss/forms')]
+};
