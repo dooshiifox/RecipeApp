1b0352b72bb82c29e5f1bc95813711c534846a9e
EDoosh <edoosh.yt@gmail.com>
2022-07-07 21:52:44 +1200

    Added a link to the Nutriblocks website on the footer.

--- web/src/lib/footer/Footer.svelte
+++ web/src/lib/footer/Footer.svelte
@@ -5,14 +5,18 @@
 	class="h-[72px] w-full flex items-center justify-center gap-20 bg-[url('/images/footer-bg.jpg')] bg-cover"
 >
 	<!-- A wrapper so the slanted background works. -->
-	<div class="relative z-10 h-full">
+	<a class="block relative z-10 h-full group" href="https://nutriblocks.co.nz/">
 		<!-- The slanted background -->
 		<div class="bg-white/70 absolute inset-0 -skew-x-12 -z-10" />
 		<div class="h-full flex px-8 gap-4 items-center">
 			<img src="/images/nutriblocks_logo.png" alt="Nutriblocks Logo" class="w-16 h-16" />
-			<span class=" text-2xl text-green-600 tracking-widest">Nutriblocks</span>
+			<span
+				class="text-2xl text-green-600 tracking-widest border-b border-b-transparent transition-colors group-hover:border-green-600"
+			>
+				Nutriblocks
+			</span>
 		</div>
-	</div>
+	</a>
 	<span class="text-lg text-white italic">
 		Copyright © 2022 Nutriblock, Ltd. All Rights Reserved.
 	</span>
